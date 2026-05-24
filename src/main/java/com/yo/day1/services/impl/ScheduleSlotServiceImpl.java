047d328a1e7267a8c7ed81053ebd487102216880
Yubi1602 <baokhatranle@gmail.com>
2026-05-24 09:17:44 +0700

    update: cập nhật thêm các chức năn

---
+++ src/main/java/com/yo/day1/services/impl/ScheduleSlotServiceImpl.java
@@ -0,0 +1,33 @@
+package com.yo.day1.services.impl;
+
+import com.yo.day1.domain.entity.ScheduleSlot;
+import com.yo.day1.repository.ScheduleSlotRepository;
+
+import com.yo.day1.services.ScheduleSlotService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ScheduleSlotServiceImpl implements ScheduleSlotService {
+    private final ScheduleSlotRepository scheduleSlotRepository;
+
+    public List<ScheduleSlot> findAll() {
+        return scheduleSlotRepository.findAll();
+    }
+
+    public Optional<ScheduleSlot> findById(Long id) {
+        return scheduleSlotRepository.findById(id);
+    }
+
+    public ScheduleSlot save(ScheduleSlot scheduleSlot) {
+        return scheduleSlotRepository.save(scheduleSlot);
+    }
+
+    public void deleteById(Long id) {
+        scheduleSlotRepository.deleteById(id);
+    }
+}
